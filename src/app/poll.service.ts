aeac6f1fe1ce6c7a0da76f1179227067be489322
Toxantron <thomas.fuchs91@gmail.com>
2019-06-27 22:06:46 +0200

    Poll service implementation

--- src/app/poll.service.ts
+++ src/app/poll.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Session } from './session';
+import { MemberVote } from './card';
+import { Observable } from 'rxjs';
+
+export class PollResponse {
+  name: string;
+  timestamp: number;
+
+  topic: string;
+  description: string;
+  url: string;
+
+  flipped: boolean;
+  consensus: boolean;
+
+  votes: MemberVote[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PollService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  currentPoll(session: Session) : Observable<PollResponse> {
+    return this.http.get<PollResponse>('/api/poll/current/' + session.id);
+  }
 }
